5943a89a2f0c19ffe581a6d995c7acaa3e751370
Haneul Shin <haneulshin1030@gmail.com>
2023-03-08 13:37:45 -0500

    comment code

--- three_machines.py
+++ three_machines.py
@@ -6,7 +6,7 @@ from _thread import *
 from threading import Thread
 from multiprocessing import Process, Queue
 
-
+# Global variables: host, ports, sockets
 host = "127.0.0.1"
 port1 = 5500
 port = [port1 + i for i in range(3)]
@@ -26,8 +26,10 @@ class VirtualMachine:
     self.queue_size = 0
 
   def run(self):
+    # Create log file
     self.log_file = open(f"logs/{self.id:n}.txt", "w")
     self.connect(host)
+
     while True:
       # Clock rate cycle - sleep until the next clock tick
       time.sleep(1 / self.clock_rate)
@@ -40,17 +42,21 @@ class VirtualMachine:
         generator = random.randint(1, 11)
         message = self.clock
         if generator == 1:
+          # Send message to first other machine
           self.send_message(0, message)
           self.clock += 1
         elif generator == 2:
+          # Send message to first other machine
           self.send_message(1, message)
           self.clock += 1
         elif generator == 3:
+          # Send message to both other machines
           self.send_message(0, message)
           self.send_message(1, message)
           self.clock += 1
-        else: # internal event
+        else: 
           self.clock += 1
+          # Log internal event
           self.log(f"Internal event value {generator} at global time {time.ctime(time.time())} and logical clock time {self.clock}.")
       else:
         message = self.incoming_messages.get()
@@ -66,10 +72,12 @@ class VirtualMachine:
     if len(self.connections) > 1:
       self.connections[id].send(str(message).encode('ascii'))
       print(f"Sending message {self.clock:n} from machine {self.id:n} to other machine #{id+1:n}.")
+      self.log(f"Sending message {self.clock:n} from machine {self.id:n} to other machine #{id+1:n}.")
     unit_test_check = (self.clock, self.id, id+1)
     return unit_test_check
 
   def receive_message(self, connection):
+    # Constantly listen for messages
     while True:
       data = connection.recv(1024).decode('ascii')
       self.incoming_messages.put(data)
@@ -103,11 +111,11 @@ class VirtualMachine:
       start_new_thread(VirtualMachine.receive_message, (self, listening_socket, ))
 
   def machine(self, host, port):
-    # config.append(os.getpid())
     listening_thread = Thread(target = self.init_machine, args = ([host, port], ))
     listening_thread.start() 
-    # add delay to initialize the server - side logic on all processes
+    # Add delay to initialize the server - side logic on all processes
     time.sleep(0.5)
+    # Start thread for machine functionality, i.e. sending and receiving messages
     sending_thread = Thread(target = self.run, args = ())
     sending_thread.start()
 
@@ -116,13 +124,17 @@ if __name__ == '__main__':
   vm = []
   p = []
 
+  # Initialize 3 machines
   for i in range(3):
     vm.append(VirtualMachine(i))
   
   time.sleep(0.5)
+
+  # Start a process for each machine
   for i in range(3):
     p.append(Process(target = VirtualMachine.machine, args = (vm[i], host, port[i])))
     p[i].start()
 
+  # Join the machines
   for i in range(3):
     p[i].join()
